a424a03c6d4acb1b33373ea4e260383a45b2ec05
Sathish Mandapaka <monadicbind@Saranagati.local>
2020-06-13 15:19:20 +0530

    Added a server to expose the API of users , we also need a Proxy for the same UserAPI and finally an application to run this

--- src/UserApi.hs
+++ src/UserApi.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
@@ -7,10 +9,22 @@ module UserApi where
 import Prelude.Compat
 import Data.Aeson (decode,encode)
 import Data.Aeson.TH (deriveJSON, defaultOptions)
+import Servant
 
 data User = User { userId :: Int , userFirstName :: String , userLastName :: String } deriving (Eq,Show)
 
 $(deriveJSON defaultOptions ''User)
+
+type UserAPI = "users" :> Get '[JSON] [User]
+
+app :: Application
+app = serve userAPI server
+
+userAPI :: Proxy UserAPI
+userAPI = Proxy
+
+server :: Server UserAPI
+server = return users
 
 users :: [User]
 users = [User 1 "S" "M" , User 2 "A" "G"]
